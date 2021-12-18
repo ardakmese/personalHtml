c974902300688e338dd6868ea6ad0544920bcdc9
ardakmese <ardakmese@gmail.com>
2021-12-18 16:06:09 +0300

    Main template almost ready:)

---
+++ script/akmese.js
@@ -0,0 +1,3 @@
+var image = document.getElementById("avatar");
+image.onmouseover = function() {  image.src = "./images/vesikalık.jpg"; }
+image.onmouseout = function() {  image.src = "./images/avatar.png"; }
