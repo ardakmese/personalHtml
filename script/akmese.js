0d9c816800642b4f7df1bb14133ba7a64ed2669b
ardakmese <ardakmese@gmail.com>
2021-12-18 21:30:09 +0300

    Some upgrades

--- script/akmese.js
+++ script/akmese.js
@@ -1,7 +1,13 @@
-var image = document.getElementById("avatar");
-image.onmouseover = function() {  image.src = "./images/vesikalık.jpg"; }
-image.onmouseout = function() {  image.src = "./images/avatar.png"; }
+function imageHover(image){
+  image.style.boxShadow = "5px 5px 8px rgb(0,0,0,0.5)";
+  image.style.transitionDuration = "0.2s";
+  image.style.cursor = "pointer";
+}
 
+function imageLeave(image){
+  image.style.boxShadow = "0px 0px 0px rgb(0,0,0,0)";
+  image.style.transitionDuration = "0.2s";
+}
 
 function getAbout() {
     location.href = 'about.html';
