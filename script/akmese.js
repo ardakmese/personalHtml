2aaa71ccaab3138ca9ec6e5675f3ad6bebd0bc88
ardakmese <ardakmese@gmail.com>
2021-12-18 20:03:37 +0300

    Commit

--- script/akmese.js
+++ script/akmese.js
@@ -1,3 +1,20 @@
 var image = document.getElementById("avatar");
 image.onmouseover = function() {  image.src = "./images/vesikalık.jpg"; }
 image.onmouseout = function() {  image.src = "./images/avatar.png"; }
+
+
+function getAbout() {
+    location.href = 'about.html';
+}
+
+function getWriteMe() {
+    location.href = 'writeme.html';
+}
+
+function getHome() {
+    location.href = 'home.html';
+}
+
+function getInterest() {
+    location.href = 'interests.html';
+}
